49e95bbebbdc41a10aba3ce46302ee5ac284c3fc
Max Gallant <mcgallant72@gmail.com>
2016-05-29 12:01:41 -0600

    added html view for all lists

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :lists
     resources :list_items
   end
+
+  get '/' => 'lists#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
